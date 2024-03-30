71ba0bd03e8e37a9d0ce170538ef482a3072d120
Ivan <mr.komar991@gmail.com>
2024-03-30 22:46:11 +0200

    3.1EPAM

---
+++ 3C#EPAM/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("A:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("B:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("A --> B");
+if (A >= B)
+{
+    for (int i = A; i >= B; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    for (int i = B; i >= A; i--) 
+    {
+        Console.WriteLine(i);
+    }
+}
